da060a19cdd68fd815ac3ecc4688f05dece7a8ae
Ivan Alimin <aliminivan1990@gmail.com>
2024-08-17 16:52:07 +0300

    Practice task - Stream API ForkJoinPool: Recursive calculation of factorial

---
+++ src/main/java/com/ivanalimin/recursive_factorial/FactorialTask.java
@@ -0,0 +1,23 @@
+package com.ivanalimin.recursive_factorial;
+
+import java.util.concurrent.RecursiveTask;
+
+public class FactorialTask extends RecursiveTask<Long> {
+
+    private final int n;
+
+    public FactorialTask(int n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Long compute() {
+        if (n <= 1) {
+            return 1L;
+        } else {
+            FactorialTask subTask = new FactorialTask(n - 1);
+            subTask.fork();
+            return n * subTask.join();
+        }
+    }
+}
